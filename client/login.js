027864ba3d67f95acd5c79d53a659be1c1f55811
Rolandas Jasilionis <rolandas.jasilionis@gmail.com>
2025-06-03 14:31:46 +0300

    Pataisytas login.js fetch URL į 8000 portą

--- client/login.js
+++ client/login.js
@@ -10,7 +10,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
   }
 
   try {
-    const response = await fetch('http://localhost:5000/api/users/login', {
+    const response = await fetch('http://localhost:8000/api/users/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
