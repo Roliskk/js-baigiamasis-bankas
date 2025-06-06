bb80918a8da8e624ad2e2b4f5c2783f4295e606d
Rolandas Jasilionis <rolandas.jasilionis@gmail.com>
2025-06-06 21:49:08 +0300

    🔒 Pašalintas Mongo URI iš kodo, naudotas .env

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
@@ -8,24 +10,24 @@ const accountRoutes = require('./routes/accountRoutes');
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Prisijungta prie MongoDB"))
+  .catch((err) => console.error("Klaida jungiantis prie DB:", err));
+
 app.use(cors({
-  origin: 'http://127.0.0.1:5500' // Svarbu: nurodykite tikslų jūsų kliento adresą
+  origin: 'http://127.0.0.1:5500'
 }));
 app.use(express.json()); 
 app.use((req, res, next) => {
-    console.log(`Prieinanti užklausa: ${req.method} ${req.originalUrl}`);
-    console.log(`Užklausos antraštės: ${JSON.stringify(req.headers, null, 2)}`);
-    next(); 
+  console.log(`Prieinanti užklausa: ${req.method} ${req.originalUrl}`);
+  console.log(`Užklausos antraštės: ${JSON.stringify(req.headers, null, 2)}`);
+  next(); 
 });
 
 app.use('/api/users', userRoutes);
 app.use('/api/accounts', accountRoutes);
 
 app.use(express.static(path.join(__dirname, '../client')));
-
-mongoose.connect('mongodb+srv://rolandasjasilionis:efJaYrCHbQvzLfQG@cluster0.6ui2e7u.mongodb.net/bankas?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => console.log('Prisijungta prie MongoDB'))
-  .catch(err => console.error('Klaida jungiantis prie MongoDB:', err));
 
 const PORT = 8000;
 app.listen(PORT, () => {
